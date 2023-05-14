d36bf6cf38d59ed6cc84da097e6af468d87cb6c7
Marc Frappier <marc.frappier@gmail.com>
2023-05-13 23:39:26 -0500

    Added user import with keyboard to Player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,7 +10,7 @@ public class Player : MonoBehaviour
     //Optional assigned value
 
     [SerializeField]
-    private float speed = 3.5f;
+    private float _speed = 3.5f;
 
    
     // Start is called before the first frame update
@@ -24,7 +24,11 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
         //transform.Translate(new Vector3(1, 0, 0));
-        transform.Translate(Vector3.right * speed * Time.deltaTime);
+        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
     }
 }
