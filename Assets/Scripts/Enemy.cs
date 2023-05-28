65d047eb4796d25b2872aaca3548c19ff41d1379
Marc Frappier <marc.frappier@gmail.com>
2023-05-28 13:28:23 -0500

    added player damage, enemy and laser destruction

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _enemySpeed = 4.0f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 6.5f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * _enemySpeed * Time.deltaTime);
+
+        if(transform.position.y < -6.5f)
+        {
+            float randomX = Random.Range(-8f, 8f);
+            transform.position = new Vector3(randomX, 6.4f, 0);
+            
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Player player = other.transform.GetComponent<Player>();
+            
+            if(player != null)
+            {
+                player.Damage();
+            }
+            Destroy(this.gameObject);
+        }
+        else if(other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Player_bak.cs
+++ Player_bak.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
+    //Public or Private
+    //Have a data type: int, float, bool, string
+    //have to have a name
+    //Optional assigned value
+
     [SerializeField]
     private float _speed = 3.5f;
     [SerializeField]
@@ -19,6 +23,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Move player to starting position at start
         transform.position = new Vector3(0, 0, 0);
         
     }
@@ -35,16 +40,46 @@ public class Player : MonoBehaviour
 
     }
 
+
+
     //Create a method to hold all that is movement related
     void CalculateMovement()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-                
+
+        //transform.Translate(new Vector3(1, 0, 0));
+        //transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        //transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+        //or
+        //transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * _speed * Time.deltaTime);
+        //or
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);   //putting the direction into a variable to use in the Translate
         transform.Translate(direction * _speed * Time.deltaTime);
 
+        //Put boundaries around the screen so they can't go off screen
+        //Restricting on the X axis
+        //if(transform.position.x > 9.3f)
+        //{
+        //    transform.position = new Vector3(9.3f, transform.position.y, 0);
+        //}
+        //else if(transform.position.x <= -9.3f)
+        //{
+        //    transform.position = new Vector3(-9.3f, transform.position.y, 0);
+        //}
+
+        //Restricting on the Y axis
+        //if(transform.position.y > 1.0f)
+        //{
+        //    transform.position = new Vector3(transform.position.x, 1.0f, 0);
+        //}
+        //else if(transform.position.y <= -4.8f)
+        //{
+        //    transform.position = new Vector3(transform.position.x, -4.8f, 0);
+        //}
+
         //Restricting on the Y axis using Math.Clamp
+
         transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -4.8f, 1.5f), 0);
 
         //Making your player wrap on the X axis
